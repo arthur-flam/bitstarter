34242eb8444cfdc30f22f38182e93a8ad49e0161
= <=>
2013-07-31 19:43:59 +0000

    added custom message

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  txt = fs.readFileSync('index.html').toString()
+  response.send(txt);
 });
 
 var port = process.env.PORT || 5000;
